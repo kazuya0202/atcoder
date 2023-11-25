08b73a33b7ae8dfd34fa4dc6b507960f98e92dc8
ichiya <kazuya7187@gmail.com>
2023-11-26 02:54:50 +0900

    abc322

--- abc322/B/main.py
+++ abc322/B/main.py
@@ -1,8 +1,18 @@
 def main():
-    # n = int(input())
-    # n, m = map(int, input().split())
-    # list_ = list(map(int, input().split()))
-    pass
+    n, m = map(int, input().split())
+    s = input()
+    t = input()
+
+    is_prefix = t.startswith(s)
+    is_postfix = t.endswith(s)
+    if is_prefix and is_postfix:
+        print("0")
+    elif is_prefix:
+        print("1")
+    elif is_postfix:
+        print("2")
+    else:
+        print("3")
 
 
 if __name__ == "__main__":
--- abc322/C/main.py
+++ abc322/C/main.py
@@ -1,8 +1,16 @@
 def main():
-    # n = int(input())
-    # n, m = map(int, input().split())
-    # list_ = list(map(int, input().split()))
-    pass
+    n, m = map(int, input().split())
+    firework_days = list(map(int, input().split()))
+
+    cnt = 0
+
+    for i in range(1, n + 1):
+        day = firework_days[cnt]
+        if i < day:
+            print(day - i)
+        elif i == day:
+            print("0")
+            cnt += 1
 
 
 if __name__ == "__main__":
